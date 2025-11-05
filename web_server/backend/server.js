17e7cff2244d975054b21ffa0c7e0321ff175369
benlua <benlua73@gmail.com>
2025-11-05 23:37:52 +0800

    Fix backend

--- web_server/backend/server.js
+++ web_server/backend/server.js
@@ -1,6 +1,10 @@
 import express from 'express';
 import path from 'path';
+import { fileURLToPath } from 'url';
 import WebSocket from 'ws';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 const PORT = process.env.WEBSOCKET_PORT || 8080;
